ec28bd176c460ea0a463535b2667237d8677e468
Wyze3306 <remy.grisollet@gmail.com>
2025-10-04 00:06:52 +0200

    remove recipe furnace + composer

--- src/crafting/CraftingManagerFromDataHelper.php
+++ src/crafting/CraftingManagerFromDataHelper.php
@@ -290,10 +290,10 @@ final class CraftingManagerFromDataHelper{
 			if($input === null){
 				continue;
 			}
-			$result->getFurnaceRecipeManager($furnaceType)->register(new FurnaceRecipe(
+			/*$result->getFurnaceRecipeManager($furnaceType)->register(new FurnaceRecipe(
 				$output,
 				$input
-			));
+			));*/
 		}
 
 		foreach(self::loadJsonArrayOfObjectsFile(Path::join($directoryPath, 'potion_type.json'), PotionTypeRecipeData::class) as $recipe){
