77582f74b076131fe7ed079b1ce080dbf5bb6d41
Wyze3306 <remy.grisollet@gmail.com>
2025-11-01 13:14:51 +0100

    fix maj

--- src/network/mcpe/NetworkSession.php
+++ src/network/mcpe/NetworkSession.php
@@ -89,7 +89,7 @@ use pocketmine\network\mcpe\protocol\types\AbilitiesData;
 use pocketmine\network\mcpe\protocol\types\AbilitiesLayer;
 use pocketmine\network\mcpe\protocol\types\BlockPosition;
 use pocketmine\network\mcpe\protocol\types\command\CommandData;
-use pocketmine\network\mcpe\protocol\types\command\CommandEnum;
+use pocketmine\network\mcpe\protocol\types\command\CommandHardEnum;
 use pocketmine\network\mcpe\protocol\types\command\CommandOverload;
 use pocketmine\network\mcpe\protocol\types\command\CommandParameter;
 use pocketmine\network\mcpe\protocol\types\command\CommandPermissions;
@@ -1171,8 +1171,8 @@ class NetworkSession{
 			$commandData[$command->getLabel()] = $data;
 		}
 
-		//PMMP : $this->sendDataPacket(AvailableCommandsPacketAssembler::assemble(array_values($commandData), [], []));
-		$this->sendDataPacket(AvailableCommandsPacket::create($commandData, $hardcodedEnums, $softEnums, $enumConstraints));
+		$this->sendDataPacket(AvailableCommandsPacketAssembler::assemble(array_values($commandData), $hardcodedEnums, $softEnums));
+		//$this->sendDataPacket(AvailableCommandsPacket::create($commandData, $hardcodedEnums, $softEnums, $enumConstraints));
 	}
 
 	/**
